28debbd8b557a3f7e4495e301203864499a92f66
Sachin <s.sachin94@gmail.com>
2017-03-29 17:10:32 +0530

    closes #11

---
+++ app/controllers/api/v1/searches_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::SearchesController < ApplicationController
+
+  def get_all_bus_numbers
+    @bus_numbers = BusRoute.pluck(:bus_num)
+    render json: {message: "List of all bus numbers", data: @bus_numbers, status_code: 201}
+  end
+
+  def get_routes_by_bus_num
+    @bus_route = BusRoute.find_by_bus_num(params[:bus_num])
+    render json: {message: "List of all bus stops to #{params[:bus_num]}", data: @bus_route, status_code: 201}
+  end
+
+  def get_bus_numbers_by_source_and_destination
+    @source = params[:source]
+    @destination = params[:destination]
+    @bus_routes = BusRoute.where("route @> ARRAY[?]::varchar[]", [@source, @destination])
+    render json: {message: "List of all bus numbers from #{params[:source] } to #{params[:destination]}", data: @bus_routes.pluck(:bus_num), status_code: 201}
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "api/v1/get_all_bus_numbers" => "api/v1/searches#get_all_bus_numbers"
+
+  get "api/v1/get_routes_by_bus_num/:bus_num" => "api/v1/searches#get_routes_by_bus_num"
+  
+  get "api/v1/get_bus_numbers_by_source_and_destination/:source/:destination" => "api/v1/searches#get_bus_numbers_by_source_and_destination"
+  # get "api/v1/get_routes_by_bus_num/:bus_num" => "api/v1/searches#get_routes_by_bus_num"
 end
