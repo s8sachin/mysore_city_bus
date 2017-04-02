8fd6c8b372a886a083f90cea30d6b7991119b467
Sachin <s.sachin94@gmail.com>
2017-04-02 18:59:02 +0530

    closes #13

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "api/v1/get_all_bus_numbers" => "api/v1/searches#get_all_bus_numbers"
+  get "/get_all_bus_numbers" => "api/v1/searches#get_all_bus_numbers"
 
-  get "api/v1/get_routes_by_bus_num/:bus_num" => "api/v1/searches#get_routes_by_bus_num"
+  get "/get_routes_by_bus_num/:bus_num" => "api/v1/searches#get_routes_by_bus_num"
   
-  get "api/v1/get_bus_numbers_by_source_and_destination/:source/:destination" => "api/v1/searches#get_bus_numbers_by_source_and_destination"
+  get "/get_bus_numbers_by_source_and_destination/:source/:destination" => "api/v1/searches#get_bus_numbers_by_source_and_destination"
 
-  get "api/v1/list_of_bus_on_bus_stop/:bus_stop" => "api/v1/searches#list_of_bus_on_bus_stop"
+  get "/list_of_bus_on_bus_stop/:bus_stop" => "api/v1/searches#list_of_bus_on_bus_stop"
   
 end
